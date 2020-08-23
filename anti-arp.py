9efb2201d479b0b8e7f6113286b1de128ef658ec
Sambhav Saggi <17993169+sambhavsaggi@users.noreply.github.com>
2020-08-23 10:41:12 -0400

    Add debug and requirements

--- anti-arp.py
+++ anti-arp.py
@@ -14,9 +14,12 @@ from scapy.all import ARP, send
 # https://stackoverflow.com/questions/1026431/cross-platform-way-to-check-admin-rights-in-a-python-script-under-windows/1026626#1026626
 def is_admin():
     """Checks if the program is running as admin"""
+    logging.debug("Checking if running as admin...")
     try:
+        logging.info("UNIX check...")
         return os.getuid() == 0
     except AttributeError:
+        logging.info("UNIX check failed! Fallback to DOS check.")
         return ctypes.windll.shell32.IsUserAnAdmin() != 0
 
 
@@ -24,26 +27,36 @@ def is_admin():
 def handler(signal_received, frame):
     """Handles SIGINT gracefully"""
     # Handle any cleanup here
+    logging.info("SIGINT received")
+    logging.debug(signal_received)
+    logging.debug(frame)
     print('SIGINT or CTRL-C detected. Exiting gracefully')
     sys.exit(0)
 
 
 def read_config(filename):
     """Reads and parses config file with format \"IP MAC\" with the first line being the router"""
+    logging.info("Opening config file %s", (filename))
     config_file = open(filename, "r")
+    logging.info("Opened config file %s", (filename))
     output = []
     lines = config_file.read().splitlines()
+    logging.info("Read config")
+    logging.debug(lines)
     for line in lines:
         output.append(line.split(' '))
     config_file.close()
+    logging.debug(output)
     return output
 
 
 def send_packets(router_ip, client_ip, router_mac, client_mac):
     """Sends ARP packets to router and client"""
     arpspoofed = ARP(op=2, psrc=router_ip, pdst=client_ip, hwdst=router_mac)
+    logging.debug(arpspoofed)
     send(arpspoofed)
     arpspoofed = ARP(op=2, psrc=client_ip, pdst=router_ip, hwdst=client_mac)
+    logging.debug(arpspoofed)
     send(arpspoofed)
 
 
@@ -58,8 +71,19 @@ def main():
         "config_file",
         help="config file with format \"IP MAC\" without quotes and with the router on line one"
     )
+    parser.add_argument(
+        "--verbose", "-v",
+        help="detailed output",
+        action="count",
+        default=0
+    )
     args = parser.parse_args()
+    numeric_level = min(args.verbose * 10, 50)
+    if not isinstance(numeric_level, int):
+        raise ValueError('Invalid log level')
+    logging.basicConfig(level=numeric_level)
     config = read_config(args.config_file)
+    logging.debug("Config: %s" % config)
     # The important part.
     while True:
         for i in range(1, len(config)):
